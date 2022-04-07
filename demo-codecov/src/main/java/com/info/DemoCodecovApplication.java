162d107666329c55f16658829eadeab48a76086c
husofskyzy <15997168992@163.com>
2022-04-07 21:13:37 +0800

    提交

---
+++ demo-codecov/src/main/java/com/info/DemoCodecovApplication.java
@@ -0,0 +1,13 @@
+package com.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodecovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCodecovApplication.class, args);
+    }
+
+}
