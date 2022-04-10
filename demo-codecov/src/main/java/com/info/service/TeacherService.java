74608f6635d5c0308c23792286735589571717ae
husofskyzy <15997168992@163.com>
2022-04-10 23:59:16 +0800

    Update TeacherService.java

--- demo-codecov/src/main/java/com/info/service/TeacherService.java
+++ demo-codecov/src/main/java/com/info/service/TeacherService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TeacherService {
     public String show(String name){
-        return name+" show2";
+        return name+" show teacher";
     }
 }
